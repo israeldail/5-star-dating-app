d41c0fbabad30a9c02ad5c65466cd1bcfe5f3d49
Austan123 <austan.augustin1@gmail.com>
2022-07-24 12:49:09 +0000

    api

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,12 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+    api.route('/user', methods=['POST', 'GET'])
+def handle_hello():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
