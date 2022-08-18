dc2501d9be2c63faa573c0db0ad20909ec1748c1
izzy417 <israeldail2@gmail.com>
2022-08-18 23:11:51 +0000

    added new background

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,9 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
   <div className="footer">
     <footer className="footer mt-auto py-3 text-center">
-      <p>
-        Contact Us
-      </p>
+      <p>Contact Us</p>
     </footer>
   </div>
 );
